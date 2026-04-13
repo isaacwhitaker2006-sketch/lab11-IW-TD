7aaf47d5acf13c212a07da2602cf50588e809511
TrentDiano <trent.diano@ufl.edu>
2026-04-13 18:57:46 -0400

    Trent Diano Partner 2 work done

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,19 @@
 import unittest
 from calculator import *
 
-class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+#Partner 1: Issac Whitaker
+#Partner 2: Trent Diano
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,10), 12)
+        self.assertEqual(add(-1,1), 0)
+        self.assertEqual(add(0,0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10,2), 8)
+        self.assertEqual(subtract(0,5), -5)
+        self.assertEqual(subtract(-2,-2), 0)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,21 +23,20 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(0,5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(2,8), 3)
+        self.assertAlmostEqual(logarithm(10,100), 2)     
+        self.assertAlmostEqual(logarithm(3,9), 2)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
+    def test_log_invalid_base(self):    
+        with self.assertRaises(ValueError):
+            logarithm(1,5)
+
+
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
