3230115cc127b14210dfadad717469113e0316b2
isaacwhitaker2006-sketch <isaacwhitaker2006@gmail.com>
2026-04-13 23:10:38 -0400

    calculator revised

--- calculator.py
+++ calculator.py
@@ -1,11 +1,9 @@
+#https://github.com/isaacwhitaker2006-sketch/lab11-IW-TD/edit/main/test_calculator.py
 import math
-"""
-calculator.py
-- Defines functions used to create a simple calculator
 
-One function per operation, in order.
-"""
-# First example
+# Partner 1: Isaac Whitaker
+# Partner 2: Trent Diano
+
 def square_root(a):
     try:
         if a < 0:
@@ -22,11 +20,11 @@ def hypotenuse(a, b):
 
 def add(a, b):
     return a + b
-def sub(a, b):
+def subtract(a, b):
     return a - b
-def multiply(a, b):
+def mul(a, b):
     return a * b
-def divide(a, b):
+def div(a, b):
     if b == 0:
         raise ZeroDivisionError
     return a / b
@@ -34,7 +32,5 @@ def logarithm(a, b):
     if a <= 0 or a == 1 or  b <= 0:
         raise ValueError
     return math.log(b) / math.log(a)
-def exponent(a, b):
+def exp(a, b):
     return a ** b
-
-
