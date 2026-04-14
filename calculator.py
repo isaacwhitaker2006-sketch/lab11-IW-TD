8911975d5c42cb8211752d88968704942c3f5a36
TrentDiano <trent.diano@ufl.edu>
2026-04-13 23:09:55 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -24,7 +24,7 @@ def subtract(a, b):
     return a - b
 def mul(a, b):
     return a * b
-def divide(a, b):
+def div(a, b):
     if b == 0:
         raise ZeroDivisionError
     return a / b
@@ -32,7 +32,7 @@ def logarithm(a, b):
     if a <= 0 or a == 1 or  b <= 0:
         raise ValueError
     return math.log(b) / math.log(a)
-def exponent(a, b):
+def exp(a, b):
     return a ** b
 
 
