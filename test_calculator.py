b87e1c5cf9d12fb6d9736221a990fcf09771cf1f
TrentDiano <trent.diano@ufl.edu>
2026-04-13 23:05:01 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,21 @@
 import unittest
 from calculator import *
+#https://github.com/isaacwhitaker2006-sketch/lab11-IW-TD/edit/main/test_calculator.py
+#Partner 1: Issac Whitaker
+#Partner 2: Trent Diano
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(2,10), 12)
+        self.assertEqual(add(-1,1), 0)
+        self.assertEqual(add(0,0), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(subtract(10,2), 8)
+        self.assertEqual(subtract(0,5), -5)
+        self.assertEqual(subtract(-2,-2), 0)
+
 
     ######## Partner 1
     def test_multiply(self): # 3 assertions
@@ -24,19 +31,18 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(divide(-4, 2), -2)
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(0,5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(2,8), 3)
+        self.assertAlmostEqual(logarithm(10,100), 2)     
+        self.assertAlmostEqual(logarithm(3,9), 2)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):    
+        with self.assertRaises(ValueError):
+            logarithm(1,5)
     
     ######## Partner 1
     def test_log_invalid_argument(self): # 1 assertion
